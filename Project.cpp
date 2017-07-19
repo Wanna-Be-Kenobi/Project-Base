0a4d6dabddd54b607f5149a710e635ae8dd7ab04
Wanna-Be-Kenobi <30285357+Wanna-Be-Kenobi@users.noreply.github.com>
2017-07-19 10:56:30 +0300

    I've done my first characters.

---
+++ Project.cpp
@@ -0,0 +1,312 @@
+#include "TXLib.h"
+
+int main()
+    {
+    txCreateWindow (1200, 800);
+
+
+
+    txSetFillColor (RGB (255, 255, 0));
+
+    txSetColor (RGB (84, 84, 84), 8);
+
+    txRectangle (175, 74, 294, 194);
+
+    txRectangle (143, 135, 174, 174);
+
+    txRectangle (295, 135, 325, 174);
+
+    txRectangle (190, 195, 224, 224);
+
+    txRectangle (245, 195, 279, 224);
+
+    txSetColor (TX_BLACK);
+
+    txSetFillColor (TX_BLACK);
+
+    txCircle (262, 106, 11);
+
+    txCircle (216, 106, 11);
+
+    txSetColor (RGB (0, 0, 0), 6);
+
+    txLine (219, 141, 205, 154);
+
+    txLine (214, 149, 227, 161);
+
+    txLine (227, 161, 251, 161);
+
+
+
+    txSetFillColor (RGB (0, 128, 0));
+
+    txSetColor (RGB (84, 84, 84), 8);
+
+    txRectangle (500, 71, 619, 188);
+
+    txRectangle (515, 191, 549, 220);
+
+    txRectangle (570, 191, 604, 220);
+
+    txRectangle (468, 131, 499, 170);
+
+    txRectangle (620, 131, 651, 170);
+
+    txSetFillColor (TX_BLACK);
+
+    txSetColor (TX_BLACK);
+
+    txRectangle (520, 94, 544, 109);
+
+    txRectangle (565, 94, 589, 109);
+
+    txSetColor (RGB (0, 0, 0), 6);
+
+    txLine (523, 173, 536, 159);
+
+    txLine (536, 159, 584, 159);
+
+    txLine (584, 159, 597, 173);
+
+    txLine (538, 83, 552, 92);
+
+    txLine (573, 83, 559, 92);
+
+
+
+    txSetColor (RGB (84, 84, 84), 8);
+
+    txSetFillColor (RGB (0, 0, 255));
+
+    txCircle (805, 134, 52);
+
+    txCircle (779, 205, 20);
+
+    txCircle (829, 205, 20);
+
+    txCircle (875, 156, 20);
+
+    txCircle (735, 156, 20);
+
+    txSetColor (RGB (0, 0, 0));
+
+    txSetFillColor (TX_BLACK);
+
+    txRectangle (769, 109, 793, 124);
+
+    txRectangle (809, 109, 833, 124);
+
+    txSetColor (RGB (0, 0, 0), 8);
+
+    txLine (779, 100, 793, 100);
+
+    txLine (809, 100, 825, 100);
+
+    txLine (797, 165, 819, 165);
+
+    txLine (819, 165, 815, 150);
+
+    txSetColor (RGB (160, 82, 45), 8);
+
+    txSetFillColor (RGB (160, 82, 45));
+
+    txCircle (725, 163, 10);
+
+    txLine (725, 170, 725, 234);
+
+    txSetColor (RGB (210, 105, 30), 6);
+
+    txLine (810, 155, 822, 168);
+
+    txSetColor (TX_GREY);
+
+    txSetFillColor (TX_GREY);
+
+    txCircle (822, 168, 4);
+
+    txSetColor (RGB (0, 0, 0), 8);
+
+    txSetFillColor (TX_BLACK);
+
+    txLine (815, 150, 797, 161);
+
+    txSetColor (RGB (84, 84, 84));
+
+    txSetFillColor (RGB (84, 84, 84));
+
+    txRectangle (772, 75, 837, 85);
+
+    txRectangle (790, 33, 820, 75);
+
+
+
+    txSetColor (RGB (84, 84, 84), 8);
+
+    txSetFillColor (RGB (255, 0, 255));
+
+    txRectangle (173, 262, 292, 381);
+
+    txRectangle (141, 322, 172, 361);
+
+    txRectangle (293, 322, 323, 361);
+
+    txRectangle (188, 382, 222, 411);
+
+    txRectangle (243, 382, 277, 411);
+
+    txSetColor (TX_BLACK);
+
+    txSetFillColor (TX_BLACK);
+
+    txCircle (215, 293, 11);
+
+    txCircle (260, 293, 11);
+
+    txSetColor (RGB (0, 0, 0), 6);
+
+    txLine (211, 275, 197, 289);
+
+    txLine (267, 275, 281, 289);
+
+    txSetColor (RGB (0, 0, 0), 8);
+
+    txSetFillColor (RGB (255, 0, 255));
+
+    txCircle (236, 349, 14);
+
+
+
+    txSetColor (TX_GRAY);
+
+    txSetFillColor (TX_GRAY);
+
+    txRectangle (970, 109, 989, 193);
+
+    txRectangle (1030, 109, 1049, 193);
+
+    txRectangle (974, 95, 984, 110);
+
+    txRectangle (1035, 95, 1045, 110);
+
+    txSetColor (RGB (218, 165, 32));
+
+    txSetFillColor (RGB (218, 165, 32));
+
+    txRectangle (975, 115, 1044, 189);
+
+    txSetColor (RGB (0, 0, 0), 8);
+
+    txSetFillColor (TX_BLACK);
+
+    txLine (987, 120, 987, 184);
+
+    txLine (1032, 120, 1032, 184);
+
+
+
+    txSetColor (RGB (105, 105, 105));
+
+    txSetFillColor (RGB (105, 105, 105));
+
+    txCircle (547, 337, 30);
+
+    txSetColor (RGB (105, 105, 105));
+
+    txSetFillColor (RGB (105, 105, 105));
+
+    txRectangle (629, 300, 754, 370);
+
+    txCircle (754, 335, 35);
+
+    txSetColor (TX_GRAY);
+
+    txSetFillColor (TX_GRAY);
+
+    txCircle (629, 335, 34);
+
+    txSetColor (RGB (139, 69, 19));
+
+    txSetFillColor (RGB (139, 69, 19));
+
+    txCircle (723, 362, 40);
+
+    txSetColor (TX_BLACK);
+
+    txSetFillColor (TX_BLACK);
+
+    txCircle (723, 362, 12);
+
+    txSetColor (RGB (244, 164, 96), 8);
+
+    txLine (787, 330, 824, 300);
+
+    txSetColor (TX_RED);
+
+    txSetFillColor (TX_RED);
+
+    txCircle (824, 300, 6);
+
+
+
+    txSetColor (RGB (105, 105, 105), 8);
+
+    txSetFillColor (RGB (119, 136, 153));
+
+    txRectangle (940, 280, 1019, 359);
+
+    txSetFillColor (TX_RED);
+
+    txRectangle (915, 320, 939, 339);
+
+    txRectangle (1020, 320, 1044, 339);
+
+    txRectangle (950, 360, 969, 384);
+
+    txRectangle (990, 360, 1009, 384);
+
+    txCircle (910, 329, 10);
+
+    txSetColor (TX_RED);
+
+    txRectangle (955, 300, 974, 309);
+
+    txRectangle (980, 300, 999, 309);
+
+    txSetColor (TX_BLACK);
+
+    txSetFillColor (TX_BLACK);
+
+    txRectangle (955, 335, 999, 344);
+
+    txSetColor (RGB (255, 0, 0));
+
+    txSetFillColor (RGB (255, 0, 0));
+
+    txCircle (875, 329, 9);
+
+    txSetColor (RGB (255, 0, 0), 8);
+
+    txLine (875, 324, 893, 329);
+
+    txLine (893, 329, 875, 334);
+
+    txSetColor (RGB (105, 105, 105));
+
+    txSetFillColor (RGB (105, 105, 105));
+
+    txRectangle (771, 420, 844, 459);
+
+    txSetColor (RGB (105, 105, 105), 8);
+
+    txLine (774, 423, 766, 439);
+
+    txLine (774, 455, 766, 439);
+
+    txSetColor (RGB (128, 0, 0), 8);
+
+    txLine (762, 439, 590, 440);
+
+
+
+    return 0;
+    }
