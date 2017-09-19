847345c017a9cdb215bf2f6364616ac78f9e9fa9
Wanna-Be-Kenobi <30285357+Wanna-Be-Kenobi@users.noreply.github.com>
2017-09-19 22:29:45 +0300

    Game 2.0

--- Game.cpp
+++ Game.cpp
@@ -2,23 +2,34 @@
 
 //-----------------------------------------------------------------------------
 
-void DrawXwing();
+struct Hero
+    {
+    int x, y, r, vx, vy;
+
+    int keyRight, keyLeft, keyUp, keyDown;
+    };
+
+//-----------------------------------------------------------------------------
+
+void DrawXwing (Hero* rebel);
 
 void MoveHero();
+
+void VehicleSpeed (struct Hero* rebel, int dt);
+
+void HeroControl (struct Hero* rebel);
 
 //-----------------------------------------------------------------------------
 
 int main()
     {
-    txCreateWindow (1500, 1000);
+    txCreateWindow (1920, 1080);
 
     txSetFillColor (TX_WHITE);
 
     txClear();
 
-    //MoveHero();
-
-    DrawXwing();
+    MoveHero();
 
     return 0;
     }
@@ -27,178 +38,245 @@ int main()
 
 void MoveHero()
     {
-    DrawXwing();
+    Hero xWing = { 141, 388, 4, 0, 0, 'D', 'A', 'W', 'S' };
+
+    int dt = 1;
+
+    while (! GetAsyncKeyState (VK_ESCAPE))
+        {
+        txSetFillColor (TX_WHITE);
+
+        if (! GetAsyncKeyState (VK_CONTROL)) txClear();
+
+        DrawXwing    (&xWing);
+
+        VehicleSpeed (&xWing, dt);
+
+        HeroControl  (&xWing);
+
+        txSleep(10);
+        }
     }
 
 //-----------------------------------------------------------------------------
 
-void DrawXwing()
+void DrawXwing (Hero* rebel)
     {
     txSetColor (TX_BLACK, 3);
 
     txSetFillColor (TX_WHITE);
 
-    txRectangle (91, 306, 97, 316);
+    txRectangle (rebel->x-50,  rebel->y-82, rebel->x-44,  rebel->y-72);
 
-    txRectangle (97, 307, 127, 313);
+    txRectangle (rebel->x-44,  rebel->y-81, rebel->x-14,  rebel->y-75);
 
-    txRectangle (106, 301, 130, 307);
+    txRectangle (rebel->x-35,  rebel->y-87, rebel->x-11,  rebel->y-81);
 
-    txRectangle (127, 306, 133, 316);
+    txRectangle (rebel->x-14,  rebel->y-82, rebel->x-8,   rebel->y-72);
 
-    txRectangle (113, 328, 118, 337);
+    txRectangle (rebel->x-28,  rebel->y-60, rebel->x-23,  rebel->y-51);
 
-    txRectangle (116, 322, 130, 331);
+    txRectangle (rebel->x-25,  rebel->y-66, rebel->x-11,  rebel->y-57);
 
     txSetFillColor (TX_BLACK);
 
-    txRectangle (133, 309, 157, 312);
+    txRectangle (rebel->x-8,   rebel->y-79, rebel->x+16,  rebel->y-76);
 
-    txLine (158, 310, 205, 310);
+    txLine      (rebel->x+17,  rebel->y-78, rebel->x+64,  rebel->y-78);
 
-    txLine (179, 304, 179, 316);
+    txLine      (rebel->x+38,  rebel->y-84, rebel->x+38,  rebel->y-72);
 
-    txLine (103, 313, 95, 348);
+    txLine      (rebel->x-38,  rebel->y-75, rebel->x-46,  rebel->y-40);
 
-    txLine (130, 316, 130, 348);
+    txLine      (rebel->x-11,  rebel->y-72, rebel->x-11,  rebel->y-40);
 
-    txLine (100, 328, 106, 328);
+    txLine      (rebel->x-41,  rebel->y-60, rebel->x-35,  rebel->y-60);
 
-    txLine (74, 349, 74, 355);
+    txLine      (rebel->x-67,  rebel->y-39, rebel->x-67,  rebel->y-33);
 
     txSetFillColor (TX_WHITE);
 
-    txRectangle (88, 348, 104, 357);
+    txRectangle (rebel->x-53,  rebel->y-40, rebel->x-37,  rebel->y-31);
 
-    txRectangle (82, 357, 88, 348);
+    txRectangle (rebel->x-59,  rebel->y-31, rebel->x-53,  rebel->y-40);
 
-    txRectangle (77, 348, 82, 357);
+    txRectangle (rebel->x-64,  rebel->y-40, rebel->x-59,  rebel->y-31);
 
-    txRectangle (104, 343, 116, 361);
+    txRectangle (rebel->x-37,  rebel->y-45, rebel->x-25,  rebel->y-27);
 
-    txRectangle (116, 352, 128, 361);
+    txRectangle (rebel->x-25,  rebel->y-36, rebel->x-13,  rebel->y-27);
 
-    txRectangle (116, 352, 128, 343);
+    txRectangle (rebel->x-25,  rebel->y-36, rebel->x-13,  rebel->y-45);
 
-    txRectangle (128, 343, 139, 361);
+    txRectangle (rebel->x-13,  rebel->y-45, rebel->x-2,   rebel->y-27);
 
-    txRectangle (139, 346, 144, 358);
+    txRectangle (rebel->x-2,   rebel->y-42, rebel->x+3,   rebel->y-30);
 
-    txLine (130, 361, 130, 372);
+    txLine      (rebel->x-11,  rebel->y-27, rebel->x-11,  rebel->y-16);
 
-    txLine (130, 372, 88, 372);
+    txLine      (rebel->x-11,  rebel->y-16, rebel->x-53,  rebel->y-16);
 
-    txLine (91, 357, 86, 376);
+    txLine      (rebel->x-50,  rebel->y-31, rebel->x-55,  rebel->y-12);
 
-    txLine (86, 376, 86, 399);
+    txLine      (rebel->x-55,  rebel->y-12, rebel->x-55,  rebel->y+11);
 
-    txLine (130, 372, 232, 382);
+    txLine      (rebel->x-11,  rebel->y-16, rebel->x+91,  rebel->y-6);
 
-    txLine (232, 381, 232, 396);
+    txLine      (rebel->x+91,  rebel->y-7,  rebel->x+91,  rebel->y+8);
 
-    txLine (232, 381, 260, 384);
+    txLine      (rebel->x+91,  rebel->y-7,  rebel->x+119, rebel->y-4);
 
-    txLine (260, 384, 262, 385);
+    txLine      (rebel->x+119, rebel->y-4,  rebel->x+121, rebel->y-3);
 
-    txLine (262, 385, 263, 387);
+    txLine      (rebel->x+121, rebel->y-3,  rebel->x+122, rebel->y-1);
 
-    txLine (263, 387, 263, 389);
+    txLine      (rebel->x+122, rebel->y-1,  rebel->x+122, rebel->y+1);
 
-    txLine (263, 389, 262, 391);
+    txLine      (rebel->x+122, rebel->y+1,  rebel->x+121, rebel->y+3);
 
-    txLine (262, 391, 260, 393);
+    txLine      (rebel->x+121, rebel->y+3,  rebel->x+119, rebel->y+5);
 
-    txLine (260, 393, 232, 396);
+    txLine      (rebel->x+119, rebel->y+5,  rebel->x+91,  rebel->y+8);
 
-    txLine (232, 395, 130, 405);
+    txLine      (rebel->x+91,  rebel->y+7,  rebel->x-11,  rebel->y+17);
 
-    txLine (130, 405, 130, 415);
+    txLine      (rebel->x-11,  rebel->y+17, rebel->x-11,  rebel->y+27);
 
-    txLine (130, 405, 88, 405);
+    txLine      (rebel->x-11,  rebel->y+17, rebel-> x-53, rebel->y+17);
 
-    txLine (86, 399, 91, 420);
+    txLine      (rebel->x-55,  rebel->y+11, rebel->x-50,  rebel->y+32);
 
-    txRectangle (88, 420, 104, 429);
+    txRectangle (rebel->x-53,  rebel->y+32, rebel->x-37,  rebel->y+41);
 
-    txRectangle (88, 420, 82, 429);
+    txRectangle (rebel->x-53,  rebel->y+32, rebel->x-59,  rebel->y+41);
 
-    txRectangle (82, 429, 77, 420);
+    txRectangle (rebel->x-59,  rebel->y+41, rebel->x-64,  rebel->y+32);
 
-    txLine (74, 421, 74, 427);
+    txLine      (rebel->x-67,  rebel->y+33, rebel->x-67,  rebel->y+39);
 
-    txRectangle (104, 433, 116, 415);
+    txRectangle (rebel->x-37,  rebel->y+45, rebel->x-25,  rebel->y+27);
 
-    txRectangle (116, 415, 128, 424);
+    txRectangle (rebel->x-25,  rebel->y+27, rebel->x-13,  rebel->y+36);
 
-    txRectangle (116, 433, 128, 424);
+    txRectangle (rebel->x-25,  rebel->y+45, rebel->x-13,  rebel->y+36);
 
-    txRectangle (128, 415, 139, 433);
+    txRectangle (rebel->x-13,  rebel->y+27, rebel->x-2,   rebel->y+45);
 
-    txRectangle (139, 430, 144, 418);
+    txRectangle (rebel->x-2,   rebel->y+42, rebel->x+3,   rebel->y+30);
 
-    txLine (95, 430, 103, 463);
+    txLine      (rebel->x-46,  rebel->y+42, rebel->x-38,  rebel->y+75);
 
-    txLine (130, 433, 130, 462);
+    txLine      (rebel->x-11,  rebel->y+45, rebel->x-11,  rebel->y+74);
 
-    txRectangle (113, 439, 118, 448);
+    txRectangle (rebel->x-28,  rebel->y+51, rebel->x-23,  rebel->y+60);
 
-    txRectangle (130, 454, 116, 444);
+    txRectangle (rebel->x-11,  rebel->y+66, rebel->x-25,  rebel->y+56);
 
-    txLine (100, 448, 106, 448);
+    txLine      (rebel->x-41,  rebel->y+60, rebel->x-35,  rebel->y+60);
 
-    txRectangle (91, 462, 97, 471);
+    txRectangle (rebel->x-50,  rebel->y+74, rebel->x-44,  rebel->y+83);
 
-    txRectangle (97, 463, 127, 469);
+    txRectangle (rebel->x-44,  rebel->y+75, rebel->x-14,  rebel->y+81);
 
-    txRectangle (127, 462, 133, 471);
+    txRectangle (rebel->x-14,  rebel->y+74, rebel->x-8,   rebel->y+83);
 
-    txRectangle (106, 469, 130, 475);
+    txRectangle (rebel->x-35,  rebel->y+81, rebel->x-11,  rebel->y+87);
 
     txSetFillColor (TX_BLACK);
 
-    txRectangle (133, 465, 157, 468);
+    txRectangle (rebel->x-8,   rebel->y+77, rebel->x+16,  rebel->y+80);
 
-    txLine (158, 466, 205, 466);
+    txLine      (rebel->x+17,  rebel->y+78, rebel->x+64,  rebel->y+78);
 
-    txLine (179, 460, 179, 472);
+    txLine      (rebel->x+38,  rebel->y+72, rebel->x+38,  rebel->y+84);
 
-    txRectangle (86, 391, 130, 385);
+    txRectangle (rebel->x-55,  rebel->y+3,  rebel->x-11,  rebel->y-3);
 
-    txLine (98, 372, 98, 405);
+    txLine      (rebel->x-43,  rebel->y-16, rebel->x-43,  rebel->y+17);
 
-    txLine (122, 372, 122, 405);
+    txLine      (rebel->x-19,  rebel->y-16, rebel->x-19,  rebel->y+17);
 
-    txCircle (141, 388, 4);
+    txCircle    (rebel->x,     rebel->y,    rebel->r);
 
-    txLine (152, 382, 152, 394);
+    txLine      (rebel->x+11,  rebel->y-6,  rebel->x+11,  rebel->y+6);
 
-    txLine (152, 382, 158, 376);
+    txLine      (rebel->x+11,  rebel->y-6,  rebel->x+17,  rebel->y-12);
 
-    txLine (152, 394, 158, 400);
+    txLine      (rebel->x+11,  rebel->y+6,  rebel->x+17,  rebel->y+12);
 
-    txRectangle (158, 385, 178, 392);
+    txRectangle (rebel->x+17,  rebel->y-3,  rebel->x+37,  rebel->y+4);
 
-    txLine (178, 388, 226, 388);
+    txLine      (rebel->x+37,  rebel->y,    rebel->x+85,  rebel->y);
 
-    txLine (178, 389, 226, 389);
+    txLine      (rebel->x+37,  rebel->y+1,  rebel->x+85,  rebel->y+1);
 
-    txLine (239, 388, 254, 388);
+    txLine      (rebel->x+98,  rebel->y,    rebel->x+113, rebel->y);
 
-    txLine (239, 389, 239, 389);
+    txLine      (rebel->x+98,  rebel->y+1,  rebel->x+98,  rebel->y+1);
 
-    txLine (190, 379, 190, 398);
+    txLine      (rebel->x+49,  rebel->y-9,  rebel->x+49,  rebel-> y+10);
 
-    txLine (209, 381, 209, 396);
+    txLine      (rebel->x+68,  rebel->y-7,  rebel->x+69,  rebel->y+8);
 
     txSetColor (TX_BLACK, 2);
 
-    txLine (100, 334, 105, 334);
+    txLine      (rebel->x-41,  rebel->y-54, rebel->x-36,  rebel->y-54);
 
-    txLine (100, 453, 105, 453);
+    txLine      (rebel->x-41,  rebel->y+65, rebel->x-36,  rebel->y+65);
 
     txSetColor (TX_BLACK, 1);
 
-    txLine (100, 323, 108, 323);
+    txLine      (rebel->x-41,  rebel->y-65, rebel->x-33,  rebel->y-65);
 
-    txLine (100, 443, 108, 443);
+    txLine      (rebel->x-41,  rebel->y+55, rebel->x-33,  rebel->y+55);
+    }
+
+//-----------------------------------------------------------------------------
+
+void VehicleSpeed (Hero* rebel, int dt)
+    {
+    rebel->x = rebel->x + rebel->vx * dt;
+
+    rebel->y = rebel->y + rebel->vy * dt;
+
+    if (rebel->x > txGetExtentX() - rebel->r)
+        {
+        rebel->vx = -rebel->vx;
+
+        rebel->x = txGetExtentX() - rebel->r;
+        }
+
+    if (rebel->y > txGetExtentY() - rebel->r)
+        {
+        rebel->vy = -rebel->vy;
+
+        rebel->y = txGetExtentY() - rebel->r;
+        }
+
+    if (rebel->x < 0 + rebel->r)
+        {
+        rebel->vx = -rebel->vx;
+
+        rebel->x = 0 + rebel->r;
+        }
+
+    if (rebel->y < 0 + rebel->r)
+        {
+        rebel->vy = -rebel->vy;
+
+        rebel->y = 0 + rebel->r;
+        }
+    }
+
+//-----------------------------------------------------------------------------
+
+void HeroControl (struct Hero* rebel)
+    {
+    if (GetAsyncKeyState (rebel->keyRight)) rebel->vx += 1;
+
+    if (GetAsyncKeyState (rebel->keyLeft))  rebel->vx -= 1;
+
+    if (GetAsyncKeyState (rebel->keyUp))    rebel->vy -= 1;
+
+    if (GetAsyncKeyState (rebel->keyDown))  rebel->vy += 1;
     }
