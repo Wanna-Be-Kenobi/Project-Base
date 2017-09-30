5fae2f4ba5db5eb691c2caaeb99fda80f4425129
Wanna-Be-Kenobi <30285357+Wanna-Be-Kenobi@users.noreply.github.com>
2017-09-30 08:53:21 +0300

    He can now rotate!

--- Rotate.cpp
+++ Rotate.cpp
@@ -2,7 +2,7 @@
 
 //-----------------------------------------------------------------------------
 
-void myRectangle (int x1, int y1, double rotate, double rotateX, double rotateY);
+void myRectangle (int x1, int y1, int x2, int y2, double rotate, double rotateX, double rotateY);
 
 //-----------------------------------------------------------------------------
 
@@ -16,7 +16,7 @@ int main()
         {
         printf ("rotate = %lf\n", rotate);
 
-        myRectangle (100, 150, rotate, 400, 300);
+        myRectangle (100, 100, 200, 250, rotate, 400, 300);
 
         rotate += 0.1;
 
@@ -28,18 +28,22 @@ int main()
 
 //-----------------------------------------------------------------------------
 
-void myRectangle (int x1, int y1, double rotate, double rotateX, double rotateY)
+void myRectangle (int x1, int y1, int x2, int y2, double rotate, double rotateX, double rotateY)
     {
     txLine (400, 0, 400, 600);
 
     txLine (0, 300, 800, 300);
 
-    x1 = x1 - rotateX; // x1 = 100 -      (x1: aдресс круга в старой системе координат) -
-                       //      400 =      (rotateX: Центр новой системы координат) =
-                       //     -300        (Вычисление адреса круга по иксу в новой системе координат)
+    x1 = x1 - rotateX; // x1 = 100      (x1: aдресс круга в старой системе координат)
+                       //     -400      (rotateX: Центр новой системы координат)
+                       //_________________________________________________________________________
+                       //     -300      (Вычисление адреса круга по иксу в новой системе координат)
 
     y1 = y1 - rotateY;
 
+    x2 = x2 - rotateX;
+
+    y2 = y2 - rotateY;
 
 // Мы хотим вращать объекты вокруг точки "R"
 // Формулы могут вращать объекты только вокруг начальной точки системы координат (0, 0).
@@ -55,9 +59,23 @@ void myRectangle (int x1, int y1, double rotate, double rotateX, double rotateY)
 
     double y1r = x1 * sin (rotate) + y1 * cos (rotate) please meow
 
+    double x2r = x2 * cos (rotate) - y2 * sin (rotate);
+
+    double y2r = x2 * sin (rotate) + y2 * cos (rotate);
+
     x1 = x1r + rotateX;
 
     y1 = y1r + rotateY;
 
-    txCircle (x1, y1, 15);
+    x2 = x2r + rotateX;
+
+    y2 = y2r + rotateY;
+
+    txLine (x1, y1, x2, y1);
+
+    txLine (x1, y1, x1, y2);
+
+    txLine (x2, y1, x2, y2);
+
+    txLine (x1, y2, x2, y2);
     }
